fde01e37d0d20ae8fc47b6d9d9d563db8f70b042
Kate Cooper <kate.cooper@mac.com>
2013-12-07 18:28:00 +0000

    updating 'View All Posts' to show only posts that user is following

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -52,28 +52,36 @@ class posts_controller extends base_controller {
 	-------------------------------------------------------------------------------------------------*/
 
 	public function index() {
-
-    	# Set up the View
-		$this->template->content = View::instance('v_posts_index');
-		$this->template->title   = "Posts";
-
-		# Build the query
-		$q = "SELECT 
-            	posts .* , 
-				users.first_name, 
-				users.last_name
-			FROM posts
-			INNER JOIN users 
-            	ON posts.user_id = users.user_id";
-
-		# Run the query
+	
+	 	# Set up view
+	 	$this->template->content = View::instance('v_posts_index');
+		
+		# Set up Query
+		$q = 'SELECT 
+					posts.content,
+					posts.created,
+					posts.user_id AS post_user_id,
+					users_users.user_id AS follower_id,
+					users.first_name,
+					users.last_name,
+					users.avatar
+				FROM posts
+				INNER JOIN users_users 
+					ON posts.user_id = users_users.user_id_followed
+				INNER JOIN users 
+					ON posts.user_id = users.user_id
+				WHERE users_users.user_id = '.$this->user->user_id.'
+				ORDER BY post_id DESC';
+							
+		# Run Query
 		$posts = DB::instance(DB_NAME)->select_rows($q);
-
-		# Pass data to the View
+		
+		# Pass $posts array to the view
 		$this->template->content->posts = $posts;
-
-		# Render the View
+		
+		# Render view
 		echo $this->template;
+		
 	}
 	
 	/*-------------------------------------------------------------------------------------------------
