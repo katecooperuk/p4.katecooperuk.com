45bf89986e317372df48eaf1f07a328f366225b9
Kate Cooper <kate.cooper@mac.com>
2013-12-07 15:26:40 +0000

    added menu to view template

--- views/_v_template.php
+++ views/_v_template.php
@@ -19,9 +19,30 @@
 </head>
 
 <body>	
+	
+	<div id='menu'>
 
-	<?php if(isset($content)) echo $content; ?>
+        <a href='/'>Home</a>
 
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+
+            <a href='/users/logout'>Logout</a>
+            <a href='/users/profile'>Profile</a>
+
+        <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+
+    </div>
+
+    <br>
+
+    <?php if(isset($content)) echo $content; ?>
+    
 </body>
 </html>
