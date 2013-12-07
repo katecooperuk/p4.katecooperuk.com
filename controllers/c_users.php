bd8c7225caf832467f25a21b49fcb305928a157c
Kate Cooper <kate.cooper@mac.com>
2013-12-07 14:26:07 +0000

    updated signup function to test in post array

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -3,38 +3,25 @@ class users_controller extends base_controller {
 
     public function __construct() {
         parent::__construct();
-        echo "users_controller construct called<br><br>";
     } 
 
-    public function index() {
-        echo "This is the index page";
-    }
-
     public function signup() {
-        echo "This is the signup page";
+
+        # Setup view
+            $this->template->content = View::instance('v_users_signup');
+            $this->template->title   = "Sign Up";
+
+        # Render template
+            echo $this->template;
+
     }
 
-    public function login() {
-        echo "This is the login page";
+    public function p_signup() {
+
+        # Dump out the results of POST to see what the form submitted
+        echo '<pre>';
+        print_r($_POST);
+        echo '</pre>';          
     }
 
-    public function logout() {
-        echo "This is the logout page";
-    }
-
-    public function profile($user_name = NULL) {
-
-		$this->template->content = View::instance('v_users_profile');
-		
-		# $title is another variable used in _v_template to set the <title> of the page
-		$this->template->title = "Profile";
-
-		# Pass information to the view fragment
-		$this->template->content->user_name = $user_name;
-
-		# Render View
-		echo $this->template;
-    
-    }
-
-} # end of the class
+} # eoc
