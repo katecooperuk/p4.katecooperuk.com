45293e2ff13bcca485ffb72a44bb62c7106dba62
Kate Cooper <kate.cooper@mac.com>
2013-12-07 12:29:20 +0000

    created books controller

---
+++ controllers/c_books.php
@@ -0,0 +1,7 @@
+<?php
+class books_controller extends base_controller {
+    public function view($books_id) {         
+        # Code here to grab the books from the database using the $books_id
+    } 
+}
+?>
