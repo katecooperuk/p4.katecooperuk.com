245ff4c7421703300c6568d85598d8a8a1d2110b
Kate Cooper <kate.cooper@mac.com>
2013-12-07 14:29:32 +0000

    updated signup function - added timestamp and password token/salt

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -18,12 +18,22 @@ class users_controller extends base_controller {
 
     public function p_signup() {
 
-    	# Insert this user into the database
-		$user_id = DB::instance(DB_NAME)->insert('users', $_POST);
+    	# More data we want stored with the user
+		$_POST['created']  = Time::now();
+		$_POST['modified'] = Time::now();
+
+		# Encrypt the password  
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);            
+
+		# Create an encrypted token via their email address and a random string
+		$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string()); 
+
+		# Insert this user into the database 
+		$user_id = DB::instance(DB_NAME)->insert("users", $_POST);
 
 		# For now, just confirm they've signed up - 
 		# You should eventually make a proper View for this
-		echo 'You\'re signed up';       
+		echo 'You\'re signed up';   
     }
 
 } # eoc
