9110d50f9b7707bdffc469c096957826666e7fe8
Kate Cooper <kate.cooper@mac.com>
2013-12-07 14:45:23 +0000

    add login function / process

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -18,7 +18,7 @@ class users_controller extends base_controller {
         # Render template
             echo $this->template;
 
-    } # eoc
+    }
 
 	/*-------------------------------------------------------------------------------------------------
 	Process Signup Function
@@ -44,8 +44,70 @@ class users_controller extends base_controller {
 		echo 'You\'re signed up';   
     }
 
-} # eoc
-
 	/*-------------------------------------------------------------------------------------------------
 	Login Function
 	-------------------------------------------------------------------------------------------------*/
+	
+	public function login() {
+
+    # Setup view
+        $this->template->content = View::instance('v_users_login');
+        $this->template->title   = "Login";
+
+    # Render template
+        echo $this->template;
+	
+	}
+	
+	/*-------------------------------------------------------------------------------------------------
+	Process Login Function
+	-------------------------------------------------------------------------------------------------*/
+	
+	public function p_login() {
+
+    	# Sanitize the user entered data to prevent any funny-business (re: SQL Injection Attacks)
+		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
+
+		# Hash submitted password so we can compare it against one in the db
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
+
+		# Search the db for this email and password
+		# Retrieve the token if it's available
+		$q = "SELECT token 
+        	FROM users 
+			WHERE email = '".$_POST['email']."' 
+			AND password = '".$_POST['password']."'";
+
+		$token = DB::instance(DB_NAME)->select_field($q);
+
+		# If we didn't find a matching token in the database, it means login failed
+		if(!$token) {
+
+        	# Send them back to the login page
+			Router::redirect("/users/login/");
+
+		# But if we did, login succeeded!
+     
+    } 
+    
+    else {
+
+        /* 
+        Store this token in a cookie using setcookie()
+        Important Note: *Nothing* else can echo to the page before setcookie is called
+        Not even one single white space.
+        param 1 = name of the cookie
+        param 2 = the value of the cookie
+        param 3 = when to expire
+        param 4 = the path of the cooke (a single forward slash sets it for the entire domain)
+        */
+        setcookie("token", $token, strtotime('+1 year'), '/');
+
+        # Send them to the main page - or whever you want them to go
+        Router::redirect("/");
+
+    }
+
+}
+
+} # EOC
