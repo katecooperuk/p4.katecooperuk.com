31d225401d2e62fbd866e3ac30f159b53fb21296
Kate Cooper <kate.cooper@mac.com>
2013-12-07 16:44:39 +0000

    created posts index

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -10,6 +10,10 @@ class posts_controller extends base_controller {
         }
     }
 
+	/*-------------------------------------------------------------------------------------------------
+	Display New Post Form
+	-------------------------------------------------------------------------------------------------*/
+	
     public function add() {
 
         # Setup view
@@ -20,6 +24,10 @@ class posts_controller extends base_controller {
         echo $this->template;
 
     }
+    
+    /*-------------------------------------------------------------------------------------------------
+	Process New Posts
+	-------------------------------------------------------------------------------------------------*/
 
     public function p_add() {
 
@@ -38,5 +46,34 @@ class posts_controller extends base_controller {
         echo "Your post has been added. <a href='/posts/add'>Add another</a>";
 
     }
+    
+    /*-------------------------------------------------------------------------------------------------
+	View All Posts
+	-------------------------------------------------------------------------------------------------*/
+
+	public function index() {
+
+    	# Set up the View
+		$this->template->content = View::instance('v_posts_index');
+		$this->template->title   = "Posts";
+
+		# Build the query
+		$q = "SELECT 
+            	posts .* , 
+				users.first_name, 
+				users.last_name
+			FROM posts
+			INNER JOIN users 
+            	ON posts.user_id = users.user_id";
+
+		# Run the query
+		$posts = DB::instance(DB_NAME)->select_rows($q);
+
+		# Pass data to the View
+		$this->template->content->posts = $posts;
+
+		# Render the View
+		echo $this->template;
+	}
     
 } # EOC
