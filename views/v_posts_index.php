6a8a2b908162f3730070441bd80167c21791aabe
Kate Cooper <kate.cooper@mac.com>
2013-12-08 08:02:07 +0000

    tidying up code

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -5,7 +5,6 @@
 	<?php endif; ?>
 	
 	<?php foreach($posts as $post): ?>
-		<img class="posts" src="/uploads/avatars/<?=$post['avatar']?>" >
 		<div id="name"><?=$post['first_name']?></div>
 		<div id="post"><?=$post['content']?></div>
 		<div id="time"><?=Time::display($post['created'])?></div>
