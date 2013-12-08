080813110b2b25e588da4c2052c5d612dae05f47
Kate Cooper <kate.cooper@mac.com>
2013-12-08 09:06:27 +0000

    updated code to show links to follow users

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,15 +1,27 @@
+<?php if (count($posts) == 0) :?>
+
+	<article>
+	
+		<h4>You are not following anyone yet,<br>
+			choose some <a href="/posts/users/">Book Club Members</a> to follow
+			</h4>
+			
+	</article>
+	
+<?php endif; ?>
+
 <?php foreach($posts as $post): ?>
 
-<article>
+	<article>
 
-    <h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
+    	<h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
 
-    <p><?=$post['content']?></p>
+		<p><?=$post['content']?></p>
 
-    <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
-        <?=Time::display($post['created'])?>
-    </time>
+		<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+        	<?=Time::display($post['created'])?>
+		</time>
 
-</article>
+	</article>
 
 <?php endforeach; ?>
