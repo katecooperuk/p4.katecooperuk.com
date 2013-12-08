52a46977fbf5362542454a3911654587ce748221
Kate Cooper <kate.cooper@mac.com>
2013-12-08 08:01:45 +0000

    tidying up code

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,15 +1,14 @@
-<?php foreach($posts as $post): ?>
-
-<article>
-
-    <h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
-
-    <p><?=$post['content']?></p>
-
-    <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
-        <?=Time::display($post['created'])?>
-    </time>
-
-</article>
-
-<?php endforeach; ?>
+	<?php if (count($posts) == 0) :?>
+		<h4>You are not following anyone yet,<br>
+			choose some <a href="/posts/users/">ChatterBoxes</a>to follow
+		</h4>
+	<?php endif; ?>
+	
+	<?php foreach($posts as $post): ?>
+		<img class="posts" src="/uploads/avatars/<?=$post['avatar']?>" >
+		<div id="name"><?=$post['first_name']?></div>
+		<div id="post"><?=$post['content']?></div>
+		<div id="time"><?=Time::display($post['created'])?></div>
+		<br>
+		
+	<?php endforeach; ?>
