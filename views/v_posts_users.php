140ae6092b8b7808a0874fbbb1b677be442b0717
Kate Cooper <kate.cooper@mac.com>
2013-12-08 08:02:13 +0000

    tidying up code

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -3,7 +3,7 @@
 	<div id="name"><?=$user['first_name']?> <?=$user['last_name']?></div>
 	
 	<?php if(isset($connections[$user['user_id']])): ?>
-		a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+		<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 	<?php else: ?>
 		<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
 	<?php endif; ?>
