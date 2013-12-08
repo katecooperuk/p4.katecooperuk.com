b08cb3a8c996a8f54e159558e5131db3de12705b
Kate Cooper <kate.cooper@mac.com>
2013-12-08 08:08:10 +0000

    adding error checking to signup

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,21 +1,32 @@
 <form method='POST' action='/users/p_signup'>
 
-    First Name<br>
-    <input type='text' name='first_name'>
-    <br><br>
-
-    Last Name<br>
-    <input type='text' name='last_name'>
-    <br><br>
-
-    Email<br>
-    <input type='text' name='email'>
-    <br><br>
-
-    Password<br>
-    <input type='password' name='password'>
-    <br><br>
-
-    <input type='submit' value='Sign up'>
+	First Name<br>
+	<input type='text' name='first_name'>
+	<br><br>
+	
+	Last Name<br>
+	<input type='text' name='last_name'>
+	<br><br>
+	
+	Email<br>
+	<input type='text' name='email'>
+	<br><br>
+	
+	Password<br>
+	<input type='password' name='password'>
+	<br><br>
+	
+	<input type='submit' value='Sign Up'>
+			
+		<?php if(isset($error) && $error == 'blank-fields'): ?>
+			<div class='error'>
+				All fields need to be completed
+			</div>
+		
+		<?php elseif(isset($error) && $error == 'invalid-login'): ?>
+			<div class='error'>
+				Invalid Login, please try again
+			</div>
+		<?php endif; ?>
 
 </form>
