8511b922f43579dc8751b00b43a5c7731c3e8401
Kate Cooper <kate.cooper@mac.com>
2013-12-08 08:34:15 +0000

    fixing code back to earlier

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,12 +1,19 @@
 <?php foreach($users as $user): ?>
 
-	<div id="name"><?=$user['first_name']?> <?=$user['last_name']?></div>
-	
-	<?php if(isset($connections[$user['user_id']])): ?>
-		<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
-	<?php else: ?>
-		<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-	<?php endif; ?>
-	<br><br>
+    <!-- Print this user's name -->
+    <?=$user['first_name']?> <?=$user['last_name']?>
+    
+    <br>
 
-<?php endforeach ?>
+    <!-- If there exists a connection with this user, show a unfollow link -->
+    <?php if(isset($connections[$user['user_id']])): ?>
+        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+
+    <!-- Otherwise, show the follow link -->
+    <?php else: ?>
+        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+    <?php endif; ?>
+
+    <br><br>
+
+<?php endforeach; ?>
