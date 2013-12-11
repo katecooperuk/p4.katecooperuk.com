23869f428b66adee38a5f54f0f908c82a9640e4a
Kate Cooper <kate.cooper@mac.com>
2013-12-11 07:28:15 +0000

    added books add form

---
+++ views/v_books_add.php
@@ -0,0 +1,17 @@
+<form name='appForm' method='POST' action='/books/p_add' onsubmit='return validateForm();'>
+
+	Book Title<br>
+	<input type='text' name='first_name'>
+	<br><br>
+	
+	Author<br>
+	<input type='text' name='last_name'>
+	<br><br>
+	
+	ISBN<br>
+	<input type='text' name='email'>
+	<br><br>
+	
+<input type='submit' value='Sign Up'>
+			
+</form>
