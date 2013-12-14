2ce1d9e41f31299a6cd6225f0afc9fc59f722200
Kate Cooper <kate.cooper@mac.com>
2013-12-14 13:08:12 +0000

    setup profile view so user's posts are displayed

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,1 +1,10 @@
-<h1>This is the profile of <?=$user->first_name?></h1>
+<h1><?=$user->first_name?>'s Profile</h1>
+
+<h2>My Posts</h2>
+	<?php foreach($posts as $post): ?>
+	
+		<div id="post"><?=$post['content']?></div>
+		<div id="time"><?=Time::display($post['created'])?></div>
+		<br>
+	
+	<?php endforeach;?>
