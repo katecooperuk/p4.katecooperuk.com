2fde2ab482d9f50d5c126a6f903026a95fc8fd3d
Kate Cooper <kate.cooper@mac.com>
2013-12-14 14:12:22 +0000

    added upload function

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -124,12 +124,38 @@ class users_controller extends base_controller {
 		else {
 
 			# Store token in cookie: (name, value, expiration, path)
-        	setcookie("token", $token, strtotime('+1 year'), '/');
+        	setcookie("token", $token, strtotime('+2 weeks'), '/');
 
 			# Send them to the main page - or whever you want them to go
 			Router::redirect("/");
 		}
 	
+	}
+	
+	/*-------------------------------------------------------------------------------------------------
+	Logout Function
+	-------------------------------------------------------------------------------------------------*/
+
+    public function logout() {
+    	
+    	# Sanitize Data Entry
+    	$_POST = DB::instance(DB_NAME)->sanitize($_POST);
+    
+    	 # Generate and save a new token for next login
+		 $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+
+		 # Create the data array we'll use with the update method
+		 # In this case, we're only updating one field, so our array only has one entry
+		 $data = Array("token" => $new_token);
+
+		 # Do the update
+		 DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+
+		 # Delete their token cookie by setting it to a date in the past - effectively logging them out
+		 setcookie("token", "", strtotime('-1 year'), '/');
+
+		 # Send them back to the main index.
+		 Router::redirect("/");
 	}
 	
 	/*-------------------------------------------------------------------------------------------------
@@ -163,26 +189,46 @@ class users_controller extends base_controller {
 	}
 	
 	/*-------------------------------------------------------------------------------------------------
-	Logout Function
-	-------------------------------------------------------------------------------------------------*/
-
-    public function logout() {
-    
-    	 # Generate and save a new token for next login
-		 $new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
-
-		 # Create the data array we'll use with the update method
-		 # In this case, we're only updating one field, so our array only has one entry
-		 $data = Array("token" => $new_token);
-
-		 # Do the update
-		 DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
-
-		 # Delete their token cookie by setting it to a date in the past - effectively logging them out
-		 setcookie("token", "", strtotime('-1 year'), '/');
-
-		 # Send them back to the main index.
-		 Router::redirect("/");
-	}
+	Process Image Upload
+	-------------------------------------------------------------------------------------------------*/   
+	public function picture() {
+	
+		# Sanitize Data Entry
+    	$_POST = DB::instance(DB_NAME)->sanitize($_POST);
+		
+        # Upload Image
+        if ($_FILES['avatar']['error'] == 0) {
+            
+            $avatar = Upload::upload($_FILES, "/uploads/avatars/", array('jpg', 'jpeg', 'gif', 'png', 'JPG', 'JPEG', 'GIF', 'PNG'), $this->user->user_id);
+ 
+            if($avatar == 'Invalid file type.') {
+                
+                # Error
+                Router::redirect('/users/profile/error'); 
+            }
+            
+            else {
+                
+                # Upload Image
+                $data = Array('avatar' => $avatar);
+                DB::instance(DB_NAME)->update('users', $data, 'WHERE user_id = '.$this->user->user_id);
+ 
+                # Resize and Save Image
+                $imageObj = new Image($_SERVER['DOCUMENT_ROOT'].'/uploads/avatars/'.$avatar);
+                $imageObj->resize(150,150,'crop');
+                $imageObj->save_image($_SERVER['DOCUMENT_ROOT'].'/uploads/avatars/'.$avatar);
+                
+            }
+        }
+        
+        else {
+        
+            # Error
+            Router::redirect("/users/profile/error");  
+        }
+ 
+        # Send to Profile Page
+        Router::redirect('/users/profile'); 
+    }  
 
 } # EOC
