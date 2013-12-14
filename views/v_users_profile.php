3519993b996b8e97b910d3f5a5101bd87fa4ec9c
Kate Cooper <kate.cooper@mac.com>
2013-12-14 18:24:14 +0000

    fixing avatar code

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,12 +1,6 @@
 <h1><?=$user->first_name?>'s Profile</h1>
 
-<?php if(isset($avatar)): ?>
 	<img src="<?=$user->avatar; ?>" class='profile'>
-
-<?php else: ?>
-	<img src='/uploads/default.gif'>
-	<div id="prompt">Upload your own avatar image</div>
-<?php endif; ?>
 
 <form method='POST' action="/users/picture/" enctype="multipart/form-data" >
 	
