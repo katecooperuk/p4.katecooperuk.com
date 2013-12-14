d3a5ec550e1fed1f97115fad8d29053665f8a6d9
Kate Cooper <kate.cooper@mac.com>
2013-12-14 08:49:13 +0000

    addBook function link to adBook view

--- controllers/c_books.php
+++ controllers/c_books.php
@@ -3,15 +3,28 @@ class books_controller extends base_controller {
     
    public function __construct() {
         parent::__construct();
-        echo "books_controller construct called<br><br>";
     }
     
     public function index() {
         echo "This is the index page";
     }
+    
+    /*-------------------------------------------------------------------------------------------------
+	Add Book Function
+	-------------------------------------------------------------------------------------------------*/
 
-	public function addBook() {
-        echo "This is add book page";
+	public function addBook($error = NULL) {
+       
+       # Setup view
+        $this->template->content = View::instance('v_books_addBook');
+        $this->template->title   = "Add Book";
+
+        # Pass data to the view
+		$this->template->content->error = $error;
+        
+        # Render the view (localhost/books/addBook)
+        echo $this->template;
+       
     }
 
     public function listBook() {
