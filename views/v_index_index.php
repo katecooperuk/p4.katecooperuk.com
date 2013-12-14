8ad87a51104d6e00926434dfffccc5f658bbad05
Kate Cooper <kate.cooper@mac.com>
2013-12-14 09:44:29 +0000

    updated welcome message

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -7,10 +7,10 @@
 	<?php else: ?>
 
 		<div id="welcome">
-			Welcome to ChatterBox.<br>
+			Welcome to the ChatterBox Book Club<br>
 			Login if you already have an account<br>
 			or sign up to create an account and<br>
-			join in the chatter!
+			join in the literary discussion.
 		</div>
 	
 	<?php endif; ?>
