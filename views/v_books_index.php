67f90710a565b7095ffc8276bacd4aaf29dcffb8
Kate Cooper <kate.cooper@mac.com>
2013-12-14 11:37:08 +0000

    updated books index viewer

--- views/v_books_index.php
+++ views/v_books_index.php
@@ -1,7 +1,14 @@
-<article>
-	
+<?php if (count($books) == 0) :?>
+	<article>
 		<h4>There are no books on the shelf yet<br>
 			<a href="/books/addBook/">Add some books to the shelf</a>
-			</h4>
-			
+		</h4>
 	</article>
+<?php endif; ?>
+
+<?php foreach($books as $post): ?>
+	<div id="title"><?=$post['title']?></div>
+	<div id="author"><?=$post['author']?></div>
+	<div id="name">Suggested by <?=$post['first_name']?></div>
+	<br>
+<?php endforeach; ?>
