020d511b937eea5f7e383690f17ce37db0639b4a
Kate Cooper <kate.cooper@mac.com>
2013-12-14 14:13:21 +0000

    added link to user's avatar in DB

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,5 +1,25 @@
 <h1><?=$user->first_name?>'s Profile</h1>
 
+<img src="<?=$user->avatar; ?>" class='profile'>
+
+<? if(empty($avatar)) $avatar = "/uploads/default.gif"; ?>
+
+<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
+	
+	<input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
+    <input type='file' accept='image' name='avatar'><br>
+    	
+    	<?php if(isset($error)): ?>
+			<div class='error'>
+				Image upload failed, please try again.
+			</div>
+			<br>
+		<?php endif; ?>
+    	
+	<input type='submit' name='submit' value='Upload Avatar'>
+	
+</form>
+
 <h2>My Posts</h2>
 	<?php foreach($posts as $post): ?>
 	
