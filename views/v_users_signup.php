9c0f78caa3e92297a1153c1ebd5c4700d200a500
Kate Cooper <kate.cooper@mac.com>
2013-12-15 09:34:33 +0000

    adding CSS to signup page

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,30 +1,33 @@
-<form name='appForm' method='POST' action='/users/p_signup' onsubmit='return validateForm();'>
+<div class="formcontent">
 
-	First Name<br>
-	<input type='text' name='first_name'>
-	<br><br>
-	
-	Last Name<br>
-	<input type='text' name='last_name'>
-	<br><br>
-	
-	Email<br>
-	<input type='text' name='email'>
-	<br><br>
-	
-	Password<br>
-	<input type='password' name='password'>
-	<br><br>
-	
-	<input type='submit' value='Sign Up'>
+	<form name='appForm' method='POST' action='/users/p_signup' onsubmit='return validateForm();'>
+
+		First Name<br>
+		<input type='text' name='first_name'>
+		<br><br>
+		
+		Last Name<br>
+		<input type='text' name='last_name'>
+		<br><br>
+		
+		Email<br>
+		<input type='text' name='email'>
+		<br><br>
+		
+		Password<br>
+		<input type='password' name='password'>
+		<br><br>
+		
+		<input type='submit' value='Sign Up'>
 			
-		<?php if(isset($error) && $error == 'blank-fields'): ?>
-			<div class='error'>
-				All fields need to be completed
-			</div>
-		<?php elseif(isset($error) && $error == 'invalid-login'): ?>
-			<div class='error'>
-				Invalid Login, please try again
-			</div>
-		<?php endif; ?>
-</form>
+			<?php if(isset($error) && $error == 'blank-fields'): ?>
+				<div class='error'>
+					All fields need to be completed
+				</div>
+			<?php elseif(isset($error) && $error == 'invalid-login'): ?>
+				<div class='error'>
+					Invalid Login, please try again
+				</div>
+			<?php endif; ?>
+	</form>
+</div>
