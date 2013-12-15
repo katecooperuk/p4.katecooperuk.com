3e6b71a0eb362bf1ba828ec202fd9b9b206155ce
Kate Cooper <kate.cooper@mac.com>
2013-12-15 10:18:04 +0000

    updated CSS for bookshelf text, added prompt for form

--- views/v_books_addBook.php
+++ views/v_books_addBook.php
@@ -1,17 +1,22 @@
-<form name='appForm' method='POST' action='/books/p_addBook'>
+<div class="formcontent">
 
-	Book Title<br>
-	<input type='text' name='title'>
-	<br><br>
+	<form name='appForm' method='POST' action='/books/p_addBook'>
 	
-	Author<br>
-	<input type='text' name='author'>
-	<br><br>
+		Book Title<br>
+		<input type='text' name='title'>
+		<br><br>
+		
+		Author<br>
+		<input type='text' name='author'>
+		<br><br>
+		
+		ISBN<br>
+		<input type='text' name='isbn'>
+		<div id="prompt">Please include ISBN to enable Google Preview link</div>
+		<br><br>
+		
+	<input type='submit' value='Add Book'>
+				
+	</form>
 	
-	ISBN<br>
-	<input type='text' name='isbn'>
-	<br><br>
-	
-<input type='submit' value='Add Book'>
-			
-</form>
+</div>
