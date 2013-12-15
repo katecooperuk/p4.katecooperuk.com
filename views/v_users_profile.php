4ec8219771f7f1b86d9e425d0447ced2504a9437
Kate Cooper <kate.cooper@mac.com>
2013-12-15 19:35:33 +0000

    image testing

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -2,7 +2,14 @@
 
 	<h2><?=$user->first_name?> <?=$user->last_name?>'s Profile</h2>
 
-		<img src="<?=$user->avatar; ?>" class='profile'>
+    <?php if(isset($avatar)): ?>
+	<img src="<?=$user->avatar; ?>" class='profile'>
+
+<?php else: ?>
+	<img src='/uploads/default.gif'>
+	<div id="prompt">Upload your own avatar image</div>
+
+<?php endif; ?>
 
 	<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
 	
