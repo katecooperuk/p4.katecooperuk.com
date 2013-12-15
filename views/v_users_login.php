37af05b56064e1e0a932c5246f0457cbcc533adb
Kate Cooper <kate.cooper@mac.com>
2013-12-15 09:27:03 +0000

    updated CSS for posts login page

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,29 +1,34 @@
-<form name='appForm' method='POST' action='/users/p_login' onsubmit='return validateForm();'>
+<div class="formcontent">
+	
+	<h2>LOG IN</h2>
 
-    Email<br>
-	<input type='text' name='email'>
+		<form name='appForm' method='POST' action='/users/p_login' onsubmit='return validateForm();'>
 
-	<br><br>
+			Email<br>
+			<input type='text' name='email'>
 
-	Password<br>
-	<input type='password' name='password'>
+			<br><br>
 
-	<br><br>
+			Password<br>
+			<input type='password' name='password'>
+
+			<br><br>
 			
-	<?php if($error && $error == 'user-exists'): ?>
-		<div class='error'>
-			This user already exists
-		</div>
-	<?php elseif(isset($error) && $error == 'blank-fields'): ?>
-		<div class='error'>
-			All fields need to be completed
-		</div>
-	<?php elseif(isset($error) && $error == 'invalid-login'): ?>
-		<div class='error'>
-			Invalid Login, please try again
-		</div>
-	<?php endif; ?>
+			<?php if($error && $error == 'user-exists'): ?>
+				<div class='error'>
+					This user already exists
+				</div>
+			<?php elseif(isset($error) && $error == 'blank-fields'): ?>
+				<div class='error'>
+					All fields need to be completed
+				</div>
+			<?php elseif(isset($error) && $error == 'invalid-login'): ?>
+				<div class='error'>
+					Invalid Login, please try again
+				</div>
+			<?php endif; ?>
 
-	<input type='submit' value='Log in'>
+			<input type='submit' value='Log in'>
 
-</form>
+		</form>
+</div>
