cd750d90f558c1d9d9a1dc700e7938508f86df01
Kate Cooper <kate.cooper@mac.com>
2013-12-15 10:25:25 +0000

    removed echo

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,28 +1,37 @@
-<h1><?=$user->first_name?>'s Profile</h1>
+<div class="content">
 
-	<img src="<?=$user->avatar; ?>" class='profile'>
+	<h2><?=$user->first_name?> <?=$user->last_name?>'s Profile</h2>
 
-<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
+	<?php if(isset($avatar)): ?>
+		<img src="<?=$user->avatar; ?>" class='profile'>
+	<?php else: ?>
+		<img src='/uploads/default.gif'>
+		<div id="prompt">Upload your own avatar image</div>
+
+	<?php endif; ?>
+
+	<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
 	
-	<input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
-    <input type='file' accept='image' name='avatar'><br>
+		<input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
+		<input type='file' accept='image' name='avatar'><br>
     	
-    	<?php if(isset($error)): ?>
-			<div class='error'>
-				Image upload failed, please try again.
-			</div>
+    		<?php if(isset($error)): ?>
+				<div class='error'>
+					Image upload failed, please try again.
+				</div>
+				<br>
+			<?php endif; ?>
+    	
+		<input type='submit' name='submit' value='Upload Avatar'>
+	
+	</form>
+
+	<h2>My Posts</h2>
+		<?php foreach($posts as $post): ?>
+	
+			<div id="post"><?=$post['content']?></div>
+			<div id="time"><?=Time::display($post['created'])?></div>
 			<br>
-		<?php endif; ?>
-    	
-	<input type='submit' name='submit' value='Upload Avatar'>
 	
-</form>
-
-<h2>My Posts</h2>
-	<?php foreach($posts as $post): ?>
-	
-		<div id="post"><?=$post['content']?></div>
-		<div id="time"><?=Time::display($post['created'])?></div>
-		<br>
-	
-	<?php endforeach;?>
+		<?php endforeach;?>
+</div>
