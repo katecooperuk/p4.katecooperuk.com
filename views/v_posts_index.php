050e163677c173e43fe2a0c5908df132b0f1a799
Kate Cooper <kate.cooper@mac.com>
2013-12-15 09:28:02 +0000

    tidying code

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -2,9 +2,7 @@
 	<?php if (count($posts) == 0) :?>
 
 		<article>
-	
-			You are not following anyone yet, choose some <a href="/posts/users/">Book Club Members</a>to follow
-			
+			You are not following anyone yet, choose some <a href="/posts/users/">Book Club Members</a>to follow			
 		</article>
 	
 	<?php endif; ?>
@@ -15,7 +13,7 @@
 			<img class="posts" src="/uploads/avatars/<?=$post['avatar']?>" >
 			<div id="name"><?=$post['first_name']?> <?=$post['last_name']?> posted:</div>
 			<div id="post"><?=$post['content']?></div>
-			<div id="time"><?=Time::display($post['created'])?></div>
+			<div id="time"><?=Time::display($post['created'])?></div>	
 		</article>
 
 	<?php endforeach; ?>
