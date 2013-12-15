59634a5a4ba5bdae7273973df0d711d201bf8545
Kate Cooper <kate.cooper@mac.com>
2013-12-15 09:26:27 +0000

    updated CSS for posts users

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,19 +1,21 @@
-<?php foreach($users as $user): ?>
+<div class="content">
+	<?php foreach($users as $user): ?>
 
-    <!-- Print this user's name -->
-    <?=$user['first_name']?> <?=$user['last_name']?>
+    	<!-- Print this user's name -->
+		<?=$user['first_name']?> <?=$user['last_name']?>
     
-    <br>
+		<br>
 
-    <!-- If there exists a connection with this user, show a unfollow link -->
-    <?php if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+		<!-- If there exists a connection with this user, show a unfollow link -->
+		<?php if(isset($connections[$user['user_id']])): ?>
+        	<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 
-    <!-- Otherwise, show the follow link -->
-    <?php else: ?>
-        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-    <?php endif; ?>
+		<!-- Otherwise, show the follow link -->
+		<?php else: ?>
+			<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+		<?php endif; ?>
 
-    <br><br>
+		<br><br>
 
-<?php endforeach; ?>
+	<?php endforeach; ?>
+</div>
