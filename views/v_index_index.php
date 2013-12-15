1e493ca81fe03758ba956be3293219b9f8d5abd4
Kate Cooper <kate.cooper@mac.com>
2013-12-15 08:44:48 +0000

    added CSS to view

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,4 +1,5 @@
-<?php if($user): 
+<div class="content-home">
+	<?php if($user): 
 		
 		# Send to Profile Page
 		Router::redirect('/users/profile');
@@ -9,8 +10,9 @@
 		<div id="welcome">
 			Welcome to the ChatterBox Book Club<br>
 			Login if you already have an account<br>
-			or sign up to create an account and<br>
-			join in the literary discussion.
+			or sign up to create an account, suggest<br>
+			books and join in the literary discussion.
 		</div>
 	
 	<?php endif; ?>
+</div>
