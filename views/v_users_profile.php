f0b3b76467d3aa2414d7030c79548931308938c3
Kate Cooper <kate.cooper@mac.com>
2013-12-15 15:20:56 +0000

    tidying up code

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -2,13 +2,7 @@
 
 	<h2><?=$user->first_name?> <?=$user->last_name?>'s Profile</h2>
 
-	<?php if(isset($avatar)): ?>
 		<img src="<?=$user->avatar; ?>" class='profile'>
-	<?php else: ?>
-		<img src='/uploads/default.gif'>
-		<div id="prompt">Upload your own avatar image</div>
-
-	<?php endif; ?>
 
 	<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
 	
