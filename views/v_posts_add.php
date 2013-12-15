8fdfddc107da319663ad55f16891906c7d190e86
Kate Cooper <kate.cooper@mac.com>
2013-12-15 09:02:43 +0000

    updated CSS for add posts page

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,9 +1,11 @@
-<form method='POST' action='/posts/p_add'>
+<div class="formcontent">
+	<form method='POST' action='/posts/p_add'>
 
-    <label for='content'>New Post:</label><br>
-    <textarea name='content' id='content'></textarea>
+    	<label for='content'>New Post:</label><br>
+		<textarea name='content' id='textbox'></textarea>
 
-    <br><br>
-    <input type='submit' value='New post'>
+		<br><br>
+		<input type='submit' value='New post'>
 
-</form>
+	</form>
+</div>
