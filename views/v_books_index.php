96f957a5aae9ae370d3bc2398003a3e611e9da4c
Kate Cooper <kate.cooper@mac.com>
2013-12-15 10:21:50 +0000

    updated page layout

--- views/v_books_index.php
+++ views/v_books_index.php
@@ -1,19 +1,22 @@
-<?php if (count($books) == 0) :?>
-	<article>
-		<h4>There are no books on the shelf yet<br>
-			<a href="/books/addBook/">Add some books to the shelf</a>
-		</h4>
-	</article>
-<?php endif; ?>
-
-<?php foreach($books as $post): ?>
-		<div id="bookrow">
-			<span id="title"><?=$post['title']?> - </span>
-			<span id="author"><?=$post['author']?></span><br>
-			<span id="member">Suggested by <?=$post['first_name']?></span><br>
-			<div id="gpop"><script language='javascript'>GBS_insertPreviewButtonPopup('ISBN:<?=$post['isbn']?>');</script></div>
-			<br>
-		</div>
-<?php endforeach; ?>
+<div class="content">	
+	<?php if (count($books) == 0) :?>
+		<article>
+			<h4>There are no books on the shelf yet<br>
+				<a href="/books/addBook/">Add some books to the shelf</a>
+			</h4>
+		</article>
+	<?php endif; ?>
+	
+	<?php foreach($books as $post): ?>
+			<div id="bookrow">
+				<span id="title"><?=$post['title']?> - </span>
+				<span id="author"><?=$post['author']?></span><br>
+				<span id="member">Suggested by <?=$post['first_name']?> <?=$post['last_name']?></span><br>
+				<div id="gpop"><script language='javascript'>GBS_insertPreviewButtonPopup('ISBN:<?=$post['isbn']?>');</script></div>
+				<br>
+			</div>
+	<?php endforeach; ?>
+	
+</div>
 
 <?=$books_addBook?>
